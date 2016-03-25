d7b860e06ff7b7cd30ff07e124254f3f36cce9f1
Andrew Russo <russspruce@gmail.com>
2016-03-25 15:02:24 -0700

    Adjusted the values of the questions and adjusted how they were added in order to generate the proper links.  It will allow players to go to a new page and get their results, adding a bit more of a technical side to it and making the quiz more interactive.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-  $("form#charQuiz").submit(function() {
+
+
+  $("button#results").click(function() {
+    $("#hide").toggle("yellow-background");
+  });
+
+  $("form#tripQuiz").submit(function() {
     event.preventDefault();
     var answer1 = parseInt($("input:radio[name=q1]:checked").val());
     var answer2 = parseInt($("input:radio[name=q2]:checked").val());
@@ -7,13 +13,23 @@ $(document).ready(function() {
     var answer4 = parseInt($("input:radio[name=q4]:checked").val());
     var answer5 = parseInt($("input:radio[name=q5]:checked").val());
     var total = 0;
+
     total = (answer1+answer2+answer3+answer4+answer5)
-    if (5 <= total && total <=10) {
-      alert("")
-    } else if (11 <= total && total <=14) {
-      alert("")
-    } else if (total === 25) {
-      alert("")
+
+    if (total === 5 || total === 4 || total === 3) {
+         // $(".resultOne")toggle();
+         // alert("This is a 3 test")
+         $(location).attr("href", "result1.html");
+       }
+     else if (total === 6 || total === 7 ) {
+         // $(".resultOne")toggle();
+         // alert("This is a 3 test")
+         $(location).attr("href", "http://google.com");
+       }
+ else if (total === 8) {
+      // $(".resultOne")toggle();
+      // alert("This is a 3 test")
+      $(location).attr("href", "http://reddit.com");
     }
   });
 });
